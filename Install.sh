8eb2b5095786127808df4b16703cccb6a697de32
vishaldodiya <vdodiya03@gmail.com>
2016-10-04 09:57:35 +0530

    Install JDK

---
+++ Install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+installed=$(apt-cache policy default-jdk | grep "Installed" | awk -F' ' '{print $2}')
+
+#JDK Installation Check Up
+
+if test $installed != "(none)"; then 
+    echo "Jdk is already installed"
+else
+    echo "Installing new JDK"
+        sudo apt-get install default-jdk
+        echo "JDK Installed"
+fi
+
+
+
+#echo $installed;
