e1a99472d9966ece54083589a35d200c9af87553
vishaldodiya <vdodiya03@gmail.com>
2016-10-05 18:29:46 +0530

    SSH public key

--- Install.sh
+++ Install.sh
@@ -5,11 +5,11 @@ installed1=$(apt-cache policy default-jdk | grep "Installed" | awk -F' ' '{print
 #JDK Installation Check Up
 
 if test $installed1 != "(none)"; then 
-    echo "Jdk is already installed"
+    echo "=>Jdk is already installed"
 else
-    echo "Installing new JDK"
+    echo "=>Installing new JDK"
         sudo apt-get install default-jdk
-        echo "JDK Installed"
+        echo "=>JDK Installed"
 fi
 
 #Installing SSH for running Hadoop in Psuedo-Distributed Mode
@@ -17,12 +17,21 @@ fi
 installed2=$(apt-cache policy ssh | grep "Installed" | awk -F' ' '{print $2}')
 
 if test $installed1 != "(none)"; then 
-    echo "SSH is already installed"
+    echo "=>SSH is already installed"
 else
-    echo "Installing new SSH and rsync"
+    echo "=>Installing new SSH and rsync"
         sudo apt-get install ssh
         sudo apt-get install rsync
-        echo "SSH & rsync are Installed"
+        echo "=>SSH & rsync are Installed"
 fi
+
+#Generating Public key to access SSH for Hadoop
+echo "=>Generating public to access SSH for Hadoop"
+echo "=>After complition of command if it shows key is already generated than press Y to overwrite"
+echo "=>If it is not generated and ask for folder name where to install than press Enter"
+
+ssh-keygen -t rsa -P ""
+
+cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorised_keys
 
 #echo $installed;
